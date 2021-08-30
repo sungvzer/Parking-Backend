e348c823c6fc284e396b5291ca6c04ef3d028851
sungvzer <garganosalvio@gmail.com>
2021-08-30 11:41:17 +0200

    Models: initialize parking slots list
    This module provides a single parking_slots list, initialized at startup
    with an environment variable, as written in the specs.

---
+++ models/parking.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from models.parking_slot import ParkingSlot
+
+load_dotenv()
+parking_slots_size = os.getenv("PARKING_SLOTS")
+
+if parking_slots_size is None:
+    parking_slots_size = 5
+elif parking_slots_size is str:
+    parking_slots_size = parking_slots_size.strip()
+
+# Make sure it is converted into a number
+parking_slots_size = int(parking_slots_size)
+
+parking_slots = [ParkingSlot(x) for x in range(parking_slots_size)]
